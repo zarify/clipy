e65ed63a6f4dad6263ec35b921e89fd16ddbf4cb
zarify <rob@headtilt.me>
2025-09-11 20:50:28 +0800

    chore: cleaning up temporary debug files

--- tmp/check_sample.mjs
+++
@@ -1,9 +0,0 @@
-import { analyzeCode } from '../src/js/ast-analyzer.js';
-
-const sample = "print(f\"Hello {input('What is your name? ')}!\")";
-
-(async () => {
-    const res = await analyzeCode(sample, 'variable_usage');
-    console.log('ANALYZE_RESULT');
-    console.log(JSON.stringify(res, null, 2));
-})();
